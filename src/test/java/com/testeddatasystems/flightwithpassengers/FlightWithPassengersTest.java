cfd10be9c23f7d2db5dbc3412ac3be7fbf00a452
paljicode <rpal28384@gmail.com>
2026-01-07 16:40:02 +0530

    John: add PassengerFlight integration tests

--- src/test/java/com/testeddatasystems/flightwithpassengers/FlightWithPassengersTest.java
+++ src/test/java/com/testeddatasystems/flightwithpassengers/FlightWithPassengersTest.java
@@ -3,6 +3,8 @@ package com.testeddatasystems.flightwithpassengers;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.io.IOException;
+
 import org.junit.jupiter.api.Test;
 
 import com.testeddatasystems.flights.Flight;
@@ -19,6 +21,26 @@ public class FlightWithPassengersTest {
 		assertEquals(1, flight.getNumberOfPassengers());
 		Passenger passenger2 = new Passenger("127-23-7991", "John Smith", "GB");
 		assertThrows(RuntimeException.class, () -> flight.addPassenger(passenger2));
+	}
+
+	@Test
+	public void testPassengerJoinsFlight() {
+		Passenger passenger = new Passenger("123-45-6789", "John Smith", "US");
+		Flight flight = new Flight("AA123", 100);
+		passenger.joinFlight(flight);
+		assertEquals(flight, passenger.getFlight());
+		assertEquals(1, flight.getNumberOfPassengers());
+	}
+
+	@Test
+	public void testAddRemovePassengers() throws IOException {
+		Passenger passenger = new Passenger("124-56-7890", "Michael Johnson", "US");
+		flight.addPassenger(passenger);
+		assertEquals(1, flight.getNumberOfPassengers());
+		assertEquals(flight, passenger.getFlight());
+		flight.removePassenger(passenger);
+		assertEquals(0, flight.getNumberOfPassengers());
+		assertEquals(null, passenger.getFlight());
 	}
 
 }
